ea1b28ff0d7fd6d1e90bf4abff3cbd65405ebccb
nakabonne <nakabonne@gmail.com>
2019-12-07 21:23:35 +0900

    Add bar

--- cmd/foo/main.go
+++ cmd/foo/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("foo")
+}
+
+func Bar() error {
+	msg := "msg"
+	return errors.New(fmt.Sprintf("error: %s", msg))
 }
